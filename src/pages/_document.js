6b9f807475b94b64be4d8ef587402559a2c979e9
Marcosskl <marcos.marsk@gmail.com>
2023-10-13 21:50:30 -0300

    add icon favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta name='description' content='Desenvolvido por Marcos Bezerra' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/saiba.ico" />
       </Head>
       <body className="bg-[url('../../public/brasil_art.jpg')] bg-cover bg-fixed bg-center" >
         <link
