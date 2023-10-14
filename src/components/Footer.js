7fe61a66bcb5426c503e6d7b6fb9bab0d290a39f
Marcosskl <marcos.marsk@gmail.com>
2023-10-13 21:31:43 -0300

    removido algumas DIV com informações irrelevantes do Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,29 +21,9 @@ const Footer = () => {
         </div>
 
         <div className='flex gap-5 justify-center'>
-          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Marcos Bezerra 211142900-26</Link>
+          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Marcos Bezerra</Link>
             <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='https://www.instagram.com/markin36/'><FaInstagram className='w-4 h-4 cursor-pointer' /></Link>
             <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='https://github.com/MarcosSkL'><FaGithub className='w-4 h-4 cursor-pointer' /></Link></span>
-
-          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Brenno Alves 212142900-11</Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaInstagram className='w-4 h-4 cursor-pointer' /></Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaGithub className='w-4 h-4 cursor-pointer' /></Link></span>
-        </div>
-
-        <div className='flex gap-5 justify-center'>
-          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Eduardo José 221142900-38</Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaInstagram className='w-4 h-4 cursor-pointer' /></Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaGithub className='w-4 h-4 cursor-pointer' /></Link></span>
-
-          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Renato Ostemberg 211142900-27</Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaInstagram className='w-4 h-4 cursor-pointer' /></Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaGithub className='w-4 h-4 cursor-pointer' /></Link></span>
-        </div>
-
-        <div className='flex gap-5 justify-center'>
-          <span className='flex gap-1 items-center'><Link className='no-underline font-bold bg-clip-text text-transparent hover:text-white bg-gradient-to-r from-slate-200 to-green-400' target='_blank' rel="noreferrer" href="https://github.com/MarcosSkL">Felipe Vieira 221142900-38</Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaInstagram className='w-4 h-4 cursor-pointer' /></Link>
-            <Link className='text-gray-400 text-current hover:text-white' target='_blank' href='#'><FaGithub className='w-4 h-4 cursor-pointer' /></Link></span>
         </div>
 
         <div className='flex gap-3 justify-stretch'>
